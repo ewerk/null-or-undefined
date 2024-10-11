05b3ea63ad0ac63cc250de7c8773964f5ead9abd
Florian Symanowski <f.symanowski@ewerk.com>
2024-10-11 14:22:03 +0200

    Introduce building an artifact

---
+++ build.js
@@ -0,0 +1,25 @@
+const esbuild = require('esbuild');
+const { dependencies } = require('./package.json');
+
+const commonConfig = {
+  entryPoints: ['./src/index.ts'],
+  target: [
+    'esnext',
+    'node20',
+  ],
+  bundle: true,
+  minify: true,
+  treeShaking: true,
+};
+
+esbuild.buildSync({
+  ...commonConfig,
+  format: 'cjs',
+  outfile: './dist/index.cjs.js',
+});
+
+esbuild.buildSync({
+  ...commonConfig,
+  format: 'esm',
+  outfile: './dist/index.esm.js',
+});
